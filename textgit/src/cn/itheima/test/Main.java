d752bb6dbdf0ed91f4bb0e88ec13b66a87e4b755
lilong <lilong@com.itheima>
2019-06-17 21:16:10 +0800

    hhhh

--- textgit/src/cn/itheima/test/Main.java
+++ textgit/src/cn/itheima/test/Main.java
@@ -3,8 +3,7 @@ package cn.itheima.test;
 public class Main {
     public static void main(String[] args) {
         System.out.println("测试....");
-        System.out.println("aaaaaaa");
-        System.out.println("asdadsd");
+
         System.out.println("hello");
     }
 }
